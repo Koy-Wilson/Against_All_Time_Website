f55fc39419893240ad5611b75d98712b97f275e7
Koy Wilson <koy.m.wilson-1@ou.edu>
2023-07-26 18:58:35 -0500

    Initial commit

---
+++ Against_All_Time/src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div/>
+      <h1>Against All Time</h1>
+      <div className="card">
+        <p>
+          This is a test HTML file for the website
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Brought to you by Wilson Inc.
+      </p>
+    </>
+  )
+}
+
+export default App
